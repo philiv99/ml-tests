5214f5b5225083a115b7d189cca949b8603a7d43
Hall, Phil <hallp@ProgressBook.com>
2019-11-07 17:24:09 -0500

    Adding Label entry to TransferLearning

--- src/pages/TransferLearning.tsx
+++ src/pages/TransferLearning.tsx
@@ -18,31 +18,96 @@ import {
   IonToolbar,
   IonHeader,
   IonCardSubtitle,
-  IonCardContent
+  IonCardContent,
+  IonInput,
+  IonList,
+  IonListHeader,
+  IonItem,
+  IonLabel,
+  IonSelect,
+  IonSelectOption
 } from '@ionic/react'
 import { Plugins, CameraResultType, CameraSource, CameraDirection  } from '@capacitor/core';
-const { Camera } = Plugins;
+import { SelectChangeEventDetail } from '@ionic/core';
+const { Camera, Storage } = Plugins;
 const ml5 = require('ml5');
 
 export interface TransferLearningProps {}
 
-export interface TransferLearningState {
-  imageUrl: any
+interface ObjectOption {
+  id: number;
+  label: string;
 }
 
+class Store {
+    
+  // JSON "set" example
+  async setObject(key: string, value: object) {
+    await Storage.set({
+      key: key,
+      value: JSON.stringify(value)
+    });
+  }
+
+  // JSON "get" example
+  async getObject(key: string) {
+    const ret = await Storage.get({ key: key });
+    return (ret.value==null?null:JSON.parse(ret.value));
+  }
+
+  async setItem(key: string, value: string) {
+    await Storage.set({
+      key: key,
+      value: value
+    });
+  }
+
+  async getItem(key: string) {
+    return await Storage.get({ key: key });
+  }
+
+  async removeItem(key: string) {
+    await Storage.remove({ key: 'name' });
+  }
+
+  async keys() {
+    return await Storage.keys();
+  }
+
+  async clear() {
+    await Storage.clear();
+  }
+}
+
+export interface TransferLearningState {
+  imageUrl: any,
+  labelOptions: Array<ObjectOption>,
+  selectedLabel: number
+}
+
+let store = new Store();
 
 export default class TransferLearningPage extends Component<TransferLearningProps, TransferLearningState>  {
 
-  imageRef: any;
+  labelInput: any;
+  LABELS: string;
 
   constructor (props:{}) {
     super(props)
     this.state = {
-      imageUrl: ""
+      imageUrl: "",
+      labelOptions: [
+      ],
+      selectedLabel: 0
     }
-    this.imageRef = React.createRef();
+    this.LABELS = "labels"
+    this.labelInput = React.createRef();
+    store.setObject(this.LABELS, []);
   }
   
+  compareWith = (o1: any, o2: any) => {
+    return o1 && o2 ? o1.id === o2.id : o1 === o2;
+  };
   
   async takePicture() {
     const image = await Camera.getPhoto({
@@ -55,6 +120,37 @@ export default class TransferLearningPage extends Component<TransferLearningProp
     this.setState({
       imageUrl: image.webPath
     })
+  }
+
+  async addLabel() {
+    if (!this.labelInput) return;
+    let newLabel = this.labelInput.current.value;
+    if (newLabel === "") return;
+    let labels = await store.getObject(this.LABELS) as Array<string>;
+    if (!labels) labels = [];
+    if (!labels.find((element) => element === newLabel)) {
+      labels.push(newLabel);
+      store.setObject(this.LABELS, labels);  
+      let newLabelOptions = this.state.labelOptions;
+      newLabelOptions.push({id: labels.length, label: newLabel})
+      this.setState({
+        labelOptions: newLabelOptions,
+        selectedLabel: labels.length
+      })
+    }
+  };
+
+  onLabelSelected = (event: CustomEvent<SelectChangeEventDetail>) => {
+    const target = event.target as HTMLInputElement;
+    var newLabel: number = Number(target.value);
+    if (newLabel) {
+      var oldLabel = this.state.selectedLabel;
+      this.setState({
+        selectedLabel: newLabel
+      })
+      var testLabel = this.state.selectedLabel;
+      console.log(" newLabel:"+newLabel+" oldLabel:"+oldLabel+" testLabel: "+testLabel);
+    }
   }
 
   render () { 
@@ -77,10 +173,32 @@ export default class TransferLearningPage extends Component<TransferLearningProp
                 <IonButton size="small" onClick={() => this.takePicture()}>
                   <IonIcon icon={aperture} />
                 </IonButton>
-                 <img  ref={this.imageRef} src={this.state.imageUrl} id="image" width="75%" height="75%" alt="" />
+                 <img src={this.state.imageUrl} id="image" width="75%" height="75%" alt="" />
             </IonCardContent>
           </IonCard>
-
+          <form action="">
+            <IonInput  ref={this.labelInput} type="text" maxlength={25} placeholder="Enter new label">
+              <IonButton size="small" onClick={() => this.addLabel()}>Add</IonButton>
+            </IonInput>
+                
+            <IonList>
+              <IonItem>
+                <IonLabel>Or select label</IonLabel>
+                <IonSelect value={this.state.selectedLabel} 
+                           compareWith={this.compareWith}
+                           onIonChange={(e) => this.onLabelSelected(e)}
+                           >
+                  {this.state.labelOptions.map((object, i) => {
+                    return (
+                      <IonSelectOption key={object.id} value={object.id}>
+                        {object.label}
+                      </IonSelectOption>
+                    );
+                  })}>
+                </IonSelect>
+              </IonItem>
+            </IonList>
+          </form>
         </IonContent>
       </IonPage>
     );
